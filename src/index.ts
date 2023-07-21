c965fcfa7dfe9bd9f896bbf63ee3047ff0b1502a
Michael Naumov <mnaoumov@gmail.com>
2023-07-21 15:44:51 -0600

    Add syntax-error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+const syntaxErrorCheck = require('syntax-error');
 
 function debuggableEval(code: string, scriptName: string = 'dynamicScript.js'): unknown {
+    const syntaxError = syntaxErrorCheck(code);
+    if (syntaxError) {
+        throw new SyntaxError(`${scriptName}:${syntaxError.line}:${syntaxError.column}\nParseError: ${syntaxError.message}`);
+    }
+
     const wrappedCode = `${code}
 //# sourceURL=${scriptName}
 //# sourceMappingURL=data:application/json;base64,${btoa(code)}`;

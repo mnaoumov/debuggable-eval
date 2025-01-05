76b441f19863a85a7a3d0fff383750e9099ff939
Michael Naumov <mnaoumov@gmail.com>
2025-01-04 23:20:44 -0600

    Use hash-based scriptName

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import syntaxErrorCheck from 'syntax-error';
 
-function debuggableEval(code: string, scriptName = 'dynamicScript.js'): unknown {
+function debuggableEval(code: string, scriptName?: string): unknown {
   const syntaxError = syntaxErrorCheck(code);
   if (syntaxError) {
     throw new SyntaxError(
@@ -8,8 +8,22 @@ function debuggableEval(code: string, scriptName = 'dynamicScript.js'): unknown 
     );
   }
 
+  if (!scriptName) {
+    scriptName = `debuggable-eval:${simpleHash(code)}`;
+  }
+
   const wrappedCode = `${code}\n//# sourceURL=${scriptName}`;
   return eval(wrappedCode);
+}
+
+function simpleHash(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0;
+  }
+  return hash;
 }
 
 export { debuggableEval };
